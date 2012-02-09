b11b9c6e2aba93b4d24285f19656b840f2494103
Jeffrey Pratt <jeff@metronomix.com>
2012-02-09 09:34:20 -0700

    Spacified the file.

--- Mvc.Jsonp/JsonpResult.cs
+++ Mvc.Jsonp/JsonpResult.cs
@@ -12,13 +12,8 @@ namespace Mvc.Jsonp
         private const string NullContextExceptionMessage = "Context is null.";
         private const string JsonpCallbackFormat = "{0}({1});";
         private const string JsonpContentType = "application/javascript";
-<<<<<<< HEAD
         private const string InvalidOperationExceptionMessage = "This request has been blocked because sensitive information could be disclosed to third party web sites when this is used in a GET request. To allow GET requests, set JsonRequestBehavior to AllowGet.";
         private const string HttpVerbGet = "get";
-=======
-    	private const string InvalidOperationExceptionMessage = "This request has been blocked because sensitive information could be disclosed to third party web sites when this is used in a GET request. To allow GET requests, set JsonRequestBehavior to AllowGet.";
-    	private const string HttpVerbGet = "get";
->>>>>>> 7c46bdcd1d1b9a0bca02b4e895604c6f079afa19
 
     	public string Callback { get; set; }
 
@@ -27,13 +22,8 @@ namespace Mvc.Jsonp
             if (context == null)
                 throw new ArgumentNullException(NullContextExceptionMessage);
 
-<<<<<<< HEAD
             if (this.JsonRequestBehavior == JsonRequestBehavior.DenyGet && context.HttpContext.Request.HttpMethod.ToLower() == HttpVerbGet)
                 throw new InvalidOperationException(InvalidOperationExceptionMessage);
-=======
-			if (this.JsonRequestBehavior == JsonRequestBehavior.DenyGet && context.HttpContext.Request.HttpMethod.ToLower() == HttpVerbGet)
-				throw new InvalidOperationException(InvalidOperationExceptionMessage);
->>>>>>> 7c46bdcd1d1b9a0bca02b4e895604c6f079afa19
 
             if (this.Callback == null)
                 throw new ArgumentNullException(NullCallbackExceptionMessage);

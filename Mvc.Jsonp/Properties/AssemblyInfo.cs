568edf8add7e9b551f26503160a1699de57dcd83
Jeffrey Pratt <jeff@metronomix.com>
2012-02-09 22:30:30 -0700

    Strongly signed each assembly.

--- Mvc.Jsonp/Properties/AssemblyInfo.cs
+++ Mvc.Jsonp/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("mvc.Jsonp")]
+[assembly: AssemblyTitle("Mvc.Jsonp")]
 [assembly: AssemblyDescription("JSONP for ASP.NET MVC")]
 [assembly: AssemblyProduct("Mvc.Jsonp")]
 [assembly: AssemblyCopyright("Copyright © Jeffrey Pratt 2012")]
 
 [assembly: Guid("6f2e8c0c-a626-43f7-a9c0-8c3bed088387")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
